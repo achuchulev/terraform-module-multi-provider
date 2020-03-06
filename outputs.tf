17f6abb599334b0f791267fcc81cfca1ed71dd81
Atanas Chuchulev <atanas.v4@gmail.com>
2020-03-06 13:46:11 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "vpc_id_east" {
-  value = module.east.vpc_id
+  value = module.vpc_east.vpc_id
 }
 
 output "vpc_name_east" {
-  value = module.east.vpc_name
+  value = module.vpc_east.vpc_name
 }
 
 output "vpc_id_west" {
-  value = module.west.vpc_id
+  value = module.vpc_west.vpc_id
 }
 
 output "vpc_name_west" {
-  value = module.west.vpc_name
+  value = module.vpc_west.vpc_name
 }
