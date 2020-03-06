340a7f4aaa1707bdf20c34868e43b19ee1a12a7e
Atanas Chuchulev <atanas.v4@gmail.com>
2020-03-06 13:20:41 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,33 @@
 provider "aws" {
   alias  = "east"
   region = "us-east-1"
+  
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 provider "aws" {
   alias  = "west"
   region = "us-west-1"
+  
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 module "vpc_east" {
   source    = "./vpc"
   providers = {
-    aws = "aws.east"
+    aws = aws.east
   }
+  cidr = "10.10.0.0/16"
+  vpc_name_tag = "east"
 }
 
 module "vpc_west" {
   source    = "./vpc"
   providers = {
-    aws = "aws.west"
+    aws = aws.west
   }
+  cidr = "10.20.0.0/16"
+  vpc_name_tag = "west"
 }
